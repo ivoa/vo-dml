baab1e00a298df110188618748dc9c571be4e02e
Paul Harrison <paul.harrison@manchester.ac.uk>
2021-12-30 16:49:26 +0000

    refactor testing to extract common functionality

---
+++ tools/gradletooling/sample/src/test/java/org/ivoa/dm/notstccoords/CoordsModelTest.java
@@ -0,0 +1,73 @@
+/*
+ * Created on 5 Nov 2021 
+ * Copyright 2021 Paul Harrison (paul.harrison@manchester.ac.uk)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License in file LICENSE
+ */ 
+
+package org.ivoa.dm.notstccoords;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+
+import org.ivoa.dm.AbstractTest;
+import org.ivoa.dm.ivoa.RealQuantity;
+import org.ivoa.dm.ivoa.Unit;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+/**
+ *  .
+ * @author Paul Harrison (paul.harrison@manchester.ac.uk) 
+ * @since 5 Nov 2021
+ */
+class CoordsModelTest extends AbstractTest {
+
+    /** logger for this class */
+    private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory
+            .getLogger(CoordsModelTest.class);
+    private CelestialPoint pos1;
+    
+    /**
+     * @throws java.lang.Exception
+     */
+    @BeforeAll
+    static void setUpBeforeClass() throws Exception {
+    }
+
+    
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+        
+        
+        RefLocation ssbc = new StdRefLocation("BARYCENTRE");
+        SpaceFrame icrs = new SpaceFrame().withEquinox("J2000").withRefPosition(ssbc).withSpaceRefFrame("ICRS");
+        
+        
+        RefLocation geocentric = new StdRefLocation("GEOCENTRIC");
+        SpaceFrame fk4 = new SpaceFrame().withEquinox("B1950").withRefPosition(geocentric).withSpaceRefFrame("FK4");
+        
+        Unit degree = new Unit("degree");
+        PhysicalCoordSpace coordspace = new SphericalCoordSpace();
+        CoordSys spacesys = new SpaceSys(coordspace, icrs);// FIXME withCoordspace missing from the generated code
+        pos1 = new CelestialPoint(new RealQuantity(45.0, degree), new RealQuantity(22.0, degree), spacesys );
+        
+        
+        
+    }
+    @Test
+    void TestCoordsXML() throws JAXBException {
+        logger.debug("Starting XML test");
+        JAXBContext jc = CoordsModel.contextFactory();
+        CoordsModel model = new CoordsModel();
+       // model.addContent(pos1); FIXME - need to think about the dtypes should be added to the top level  model object.
+        
+    }
+
+}
+
+
--- tools/gradletooling/sample/src/test/java/org/ivoa/dm/sample/catalog/SourceCatalogueTest.java
+++ tools/gradletooling/sample/src/test/java/org/ivoa/dm/sample/catalog/SourceCatalogueTest.java
@@ -7,48 +7,27 @@ import static org.ivoa.dm.sample.catalog.SkyCoordinate.createSkyCoordinate;
 import static org.ivoa.dm.sample.catalog.inner.SourceCatalogue.createSourceCatalogue;
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.io.File;
 import java.io.IOException;
-import java.io.StringReader;
-import java.io.StringWriter;
 import java.util.Arrays;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
 import javax.xml.bind.SchemaOutputResolver;
-import javax.xml.bind.Unmarshaller;
-import javax.xml.bind.ValidationEvent;
-import javax.xml.bind.util.ValidationEventCollector;
-import javax.xml.namespace.QName;
-import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Result;
-import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-import javax.xml.transform.stream.StreamSource;
 
+import org.ivoa.dm.AbstractTest;
 import org.ivoa.dm.ivoa.RealQuantity;
 import org.ivoa.dm.ivoa.Unit;
 import org.ivoa.dm.sample.SampleModel;
 import org.ivoa.dm.sample.catalog.inner.SourceCatalogue;
-//import org.javastro.ivoa.entities.jaxb.DescriptionValidator;
-//import org.javastro.ivoa.entities.jaxb.JaxbAnnotationMeta;
-import org.w3c.dom.Document;
 
 /*
  * Created on 20/08/2021 by Paul Harrison (paul.harrison@manchester.ac.uk).
  */
-class SourceCatalogueTest {
+class SourceCatalogueTest extends AbstractTest {
 
     /** logger for this class */
     private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory
@@ -120,21 +99,6 @@ class SourceCatalogueTest {
 
     }
 
-    private static class MySchemaOutputResolver extends SchemaOutputResolver {
-        public Result createOutput(String uri, String suggestedFileName)
-                throws IOException {
-            String[] parts = uri.split("/");
-
-
-            File file = new File(suggestedFileName);//new File(parts[parts.length -2]+".xsd");
-            StreamResult result = new StreamResult(file);
-            System.out.println("uri=" + uri + " " + file.getName());
-            result.setSystemId(file.toURI().toURL().toString());
-            return result;
-        }
-    };
-
-
     @org.junit.jupiter.api.Test
     void sourceCatJaxBTest() throws JAXBException, ParserConfigurationException, TransformerException, IOException {
 
@@ -158,40 +122,7 @@ class SourceCatalogueTest {
 //        assertTrue(mvalidation.valid, "errors on whole model");
 
 
-        DocumentBuilderFactory dbf = DocumentBuilderFactory
-                .newInstance();
-        dbf.setNamespaceAware(true);
-        Document doc = dbf.newDocumentBuilder().newDocument();
-        Marshaller m = jc.createMarshaller();
-        m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-        JAXBElement<SampleModel> element =  new JAXBElement<SampleModel>(new QName("http://ivoa.net/dm/models/vo-dml/xsd/sample/sample", "SampleModel"),SampleModel.class,model);//mmeta.element(model);
-        m.marshal(element, doc);
-        // Set up the output transformer
-        TransformerFactory transfac = TransformerFactory.newInstance();
-        Transformer trans = transfac.newTransformer();
-        trans.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "no");
-        trans.setOutputProperty(OutputKeys.INDENT, "yes");
-
-        // Print the DOM node
-        StringWriter sw = new StringWriter();
-        StreamResult result = new StreamResult(sw);
-        DOMSource source = new DOMSource(doc);
-        trans.transform(source, result);
-        System.out.println(sw.toString());
-
-        //try to read in again
-        Unmarshaller um = jc.createUnmarshaller();
-        ValidationEventCollector vc = new javax.xml.bind.util.ValidationEventCollector();
-        um.setEventHandler(vc);
-        JAXBElement<SampleModel> el = um.unmarshal(new StreamSource(new StringReader(sw.toString())),SampleModel.class);
-        if (vc.hasEvents()) {
-            for (ValidationEvent err : vc.getEvents()) {
-                System.err.println(err.getMessage());
-            }
-        }
-        assertTrue(!vc.hasEvents(), "reading xml back had errors");
-        SampleModel modelin = el.getValue();
-        assertNotNull(modelin);
+        SampleModel modelin = roundtripXML(jc, model, SampleModel.class); //TODO the namespace should be automatic....
         List<SourceCatalogue> lin = modelin.getContent(SourceCatalogue.class);
         assertEquals(1, lin.size());
         SourceCatalogue scin = lin.get(0);
@@ -200,57 +131,17 @@ class SourceCatalogueTest {
         AlignedEllipse perr = src.getPositionError();
         assertEquals(0.2, perr.longError);
         assertTrue(!src.getLuminosity().get(0).getFilter().getBandName().equals(
-       src.getLuminosity().get(1).getFilter().getBandName()),"failure to distiguish references");
+        src.getLuminosity().get(1).getFilter().getBandName()),"failure to distiguish references");
         
 
         SchemaOutputResolver sor = new MySchemaOutputResolver();
         System.out.println("generating schema");
         jc.generateSchema(sor);
     }
-    
+
     @org.junit.jupiter.api.Test
     void sourceCatJPATest() {
-                Map<String, String> props = new HashMap<>();
-        
-        //
-                
-               final String puname = "vodml_sample";
-
-        //derby
-        props.put("javax.persistence.jdbc.url", "jdbc:derby:memory:"+puname+";create=true");//IMPL differenrt DB for each PU to stop interactions
-//        props.put(PersistenceUnitProperties.JDBC_URL, "jdbc:derby:emerlindb;create=true;traceFile=derbytrace.out;traceLevel=-1;traceDirectory=/tmp");
-        props.put("javax.persistence.jdbc.driver", "org.apache.derby.jdbc.EmbeddedDriver");
-       // props.put(PersistenceUnitProperties.TARGET_DATABASE, "org.eclipse.persistence.platform.database.DerbyPlatform");
-
-//        //h2
-//        props.put(PersistenceUnitProperties.JDBC_URL, "jdbc:h2:mem:"+puname+";DB_CLOSE_DELAY=-1");//IMPL differenrt DB for each PU to stop interactions
-//        props.put(PersistenceUnitProperties.JDBC_DRIVER, "org.h2.Driver");
-//        props.put(PersistenceUnitProperties.TARGET_DATABASE, "org.eclipse.persistence.platform.database.H2Platform");
-//        
-//        //hsqldb
-//        props.put(PersistenceUnitProperties.JDBC_URL, "jdbc:hsqldb:mem:"+puname+";");//IMPL differenrt DB for each PU to stop interactions
-//        props.put(PersistenceUnitProperties.JDBC_DRIVER, "org.hsqldb.jdbcDriver");
-//        props.put(PersistenceUnitProperties.TARGET_DATABASE, "org.eclipse.persistence.platform.database.HSQLPlatform");
-        
-        
-       // props.put(PersistenceUnitProperties.DDL_GENERATION_MODE, PersistenceUnitProperties.DDL_BOTH_GENERATION);
-        props.put("javax.persistence.schema-generation.scripts.create-target", "test.sql");
-        props.put("javax.persistence.schema-generation.scripts.drop-target", "test-drop.sql");
-        props.put("hibernate.hbm2ddl.schema-generation.script.append", "false");
-        
-        props.put("javax.persistence.schema-generation.create-source", "metadata");
-        props.put("javax.persistence.schema-generation.database.action", "drop-and-create");
-        props.put("javax.persistence.schema-generation.scripts.action", "drop-and-create");
-        props.put("javax.persistence.jdbc.user", "");
-//        props.put(PersistenceUnitProperties.CACHE_SHARED_, "false");
-        
-     // Configure logging. FINE ensures all SQL is shown
-        //props.put(PersistenceUnitProperties.LOGGING_LEVEL, "FINEST");
-         
- 
-        javax.persistence.EntityManagerFactory emf = javax.persistence.Persistence.createEntityManagerFactory(puname, props);
-        
-        javax.persistence.EntityManager em = emf.createEntityManager();
+                javax.persistence.EntityManager em = setupDB("vodml_sample");
         em.getTransaction().begin();
         em.persist(sc);
         em.getTransaction().commit();
