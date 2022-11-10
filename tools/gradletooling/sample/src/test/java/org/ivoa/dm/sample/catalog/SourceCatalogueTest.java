f5d54fd9b2b5fd81d751466416e1c4ac291c4b6c
Paul Harrison <paul.harrison@manchester.ac.uk>
2022-11-10 16:30:28 +0000

    add extra base class to allow for multiple kinds of source catalog tests

--- tools/gradletooling/sample/src/test/java/org/ivoa/dm/sample/catalog/SourceCatalogueTest.java
+++ tools/gradletooling/sample/src/test/java/org/ivoa/dm/sample/catalog/SourceCatalogueTest.java
@@ -1,112 +1,30 @@
 package org.ivoa.dm.sample.catalog;
-
-import static org.ivoa.dm.filter.PhotometryFilter.createPhotometryFilter;
-import static org.ivoa.dm.sample.catalog.LuminosityMeasurement.createLuminosityMeasurement;
-import static org.ivoa.dm.sample.catalog.SDSSSource.createSDSSSource;
-import static org.ivoa.dm.sample.catalog.SkyCoordinate.createSkyCoordinate;
-import static org.ivoa.dm.sample.catalog.inner.SourceCatalogue.createSourceCatalogue;
 import static org.junit.jupiter.api.Assertions.*;
 
+
 import java.io.IOException;
-import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.util.Arrays;
-import java.util.Date;
 import java.util.List;
 
-import javax.persistence.Query;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
-import javax.xml.bind.SchemaOutputResolver;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.TransformerException;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 
 import org.hibernate.Session;
-import org.ivoa.dm.AbstractTest;
-import org.ivoa.dm.ivoa.RealQuantity;
-import org.ivoa.dm.ivoa.Unit;
 import org.ivoa.dm.sample.SampleModel;
-import org.ivoa.dm.sample.SampleModel.References;
 import org.ivoa.dm.sample.catalog.inner.SourceCatalogue;
-import org.ivoa.vodml.ModelManagement;
 
 /*
  * Created on 20/08/2021 by Paul Harrison (paul.harrison@manchester.ac.uk).
  */
-class SourceCatalogueTest extends AbstractTest {
+class SourceCatalogueTest extends BaseSourceCatalogueTest {
 
     /** logger for this class */
     private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory
             .getLogger(SourceCatalogueTest.class);
-    private SourceCatalogue sc;
-
-
-    @org.junit.jupiter.api.BeforeEach
-    void setUp() {
-               final Unit jansky = new Unit("Jy");
-        final Unit degree = new Unit("degree");
-        final Unit GHz= new Unit("GHz");
-        final SkyCoordinateFrame frame = new SkyCoordinateFrame().withName("J2000").withEquinox("J2000.0").withDocumentURI("http://coord.net");
-
-        final AlignedEllipse ellipseError = new AlignedEllipse(.2, .1);
-        SDSSSource sdss = new SDSSSource().withPositionError(ellipseError);// UNUSED, but just checking position error subsetting.
-        sdss.setPositionError(ellipseError);
-        AlignedEllipse theError = sdss.getPositionError();
-
-        sc = createSourceCatalogue(c -> {
-            c.name = "testCat";
-            c.entry = Arrays.asList(createSDSSSource(s -> {
-                s.name = "testSource";
-                s.classification = SourceClassification.AGN;
-                s.position = createSkyCoordinate(co -> {
-                    co.frame = frame;
-                    co.latitude = new RealQuantity(52.5, degree );
-                    co.longitude = new RealQuantity(2.5, degree );
-                });
-                s.positionError = ellipseError;//note subsetting forces compile need AlignedEllipse
-
-                s.luminosity = Arrays.asList(
-                        createLuminosityMeasurement(l ->{
-                            l.description = "lummeas";
-                            l.type = LuminosityType.FLUX;                         
-                            l.value = new RealQuantity(2.5, jansky );
-                            l.error = new RealQuantity(.25, jansky );
-                            l.filter = createPhotometryFilter(fl -> {
-                                fl.bandName ="C-Band";
-                                fl.spectralLocation = new RealQuantity(5.0,GHz);
-                                fl.dataValidityFrom = new Date();
-                                fl.dataValidityTo = new Date();
-                                fl.description = "radio band";
-                                fl.name = fl.bandName;
-                            });
-                            
-                        })
-                        ,createLuminosityMeasurement(l ->{
-                            l.description = "lummeas2";
-                            l.filter = createPhotometryFilter(fl -> {
-                                fl.bandName ="L-Band";
-                                fl.spectralLocation = new RealQuantity(1.5,GHz);
-                                fl.dataValidityFrom = new Date();
-                                fl.dataValidityTo = new Date();
-                                fl.description = "radio band";
-                                fl.name = fl.bandName;
-                            });
-                            l.type = LuminosityType.FLUX;
-                            l.value = new RealQuantity(3.5, jansky );
-                            l.error = new RealQuantity(.25, jansky );//TODO should be allowed to be null
-
-   })
-
-                        );
-            }));
-        }
-                );
-
-
-    }
-
     @org.junit.jupiter.api.Test
     void sourceCatJaxBTest() throws JAXBException, ParserConfigurationException, TransformerException, IOException {
 
@@ -137,20 +55,6 @@ class SourceCatalogueTest extends AbstractTest {
 
         System.out.println("generating schema");
         SampleModel.writeXMLSchema();
-    }
-
-    private void checkModel(List<SourceCatalogue> lin) {
-        assertEquals(1, lin.size());
-        SourceCatalogue scin = lin.get(0);
-        System.out.println(lin.get(0).getName());
-        SDSSSource src = (SDSSSource) scin.getEntry().get(0);
-        AlignedEllipse perr = src.getPositionError();
-        assertEquals(0.2, perr.longError);
-        assertTrue(!src.getLuminosity().get(0).getFilter().getBandName().equals(
-        src.getLuminosity().get(1).getFilter().getBandName()),"failure to distinguish references");
-        SkyCoordinateFrame fr = src.getPosition().getFrame();
-        assertNotNull(fr);
-        assertEquals("J2000", fr.getName());
     }
 
     @org.junit.jupiter.api.Test
