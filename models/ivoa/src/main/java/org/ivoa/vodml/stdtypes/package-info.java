4f50123f2209bafdb11b7e884db5eeb7c62d22dd
Paul Harrison <paul.harrison@manchester.ac.uk>
2023-04-03 08:35:34 +0100

    package comments for handwritten types

--- models/ivoa/src/main/java/org/ivoa/vodml/stdtypes/package-info.java
+++ models/ivoa/src/main/java/org/ivoa/vodml/stdtypes/package-info.java
@@ -1,8 +1,5 @@
 /**
- * $Id$
- */
-/**
- * .
+ * This package contains the hand written versions of the IVOA base types.
  * 
  * @author Paul Harrison (paul.harrison@manchester.ac.uk)
  * @since 23 Aug 2021
