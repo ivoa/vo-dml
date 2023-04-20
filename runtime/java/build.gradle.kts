9987f96fcc5e9cac4fc49fc7335d4ee102f8b46a
Paul Harrison <paul.harrison@manchester.ac.uk>
2023-04-20 18:19:45 +0100

    security update

--- runtime/java/build.gradle.kts
+++ runtime/java/build.gradle.kts
@@ -15,7 +15,7 @@ dependencies {
     implementation("javax.xml.bind:jaxb-api:2.3.1")
 //    implementation("org.glassfish.jaxb:jaxb-runtime:2.3.6")
     implementation("javax.persistence:javax.persistence-api:2.2")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.13.4.2")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.14.2")
     
     implementation("org.slf4j:slf4j-api:1.7.36")
     api("org.javastro:jaxbjpa-utils:0.1.2")
