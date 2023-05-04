4e69f51375247b180d3f3a9dead9cf173bee1f5a
Paul Harrison <paul.harrison@manchester.ac.uk>
2023-05-04 21:22:16 +0100

    prepare for release 0.3.19
    forgot to commit this!

---
+++ runtime/java/src/main/java/org/ivoa/vodml/testing/package-info.java
@@ -0,0 +1,9 @@
+/*
+ * created 4 May 2023
+ */
+/**
+ * Utility base classes for model testing.
+ * @author Paul Harrison (paul.harrison@manchester.ac.uk) 
+ * @since 4 May 2023
+ */
+package org.ivoa.vodml.testing;
