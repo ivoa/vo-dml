19c9b8fdf675e7038783e9fcd060f9bf7d62a169
Paul Harrison <paul.harrison@manchester.ac.uk>
2023-05-17 10:45:01 +0100

    couple of small tweaks from master that the merge did not capture

--- tools/gradletooling/sample/src/test/java/org/ivoa/dm/sample/catalog/BaseSourceCatalogueTest.java
+++ tools/gradletooling/sample/src/test/java/org/ivoa/dm/sample/catalog/BaseSourceCatalogueTest.java
@@ -21,7 +21,7 @@ import java.util.Date;
 import java.util.List;
 
 import org.ivoa.dm.ivoa.RealQuantity;
-import org.ivoa.vodml.stdtypes.Unit;
+import org.ivoa.dm.ivoa.Unit;
 import org.ivoa.dm.sample.catalog.inner.SourceCatalogue;
 import org.ivoa.vodml.testing.AbstractTest;
 
