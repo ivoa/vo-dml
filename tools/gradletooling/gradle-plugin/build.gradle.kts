0e82d1cf8a9a4e7890f53f47d8a60d7f996f9731
Paul Harrison <paul.harrison@manchester.ac.uk>
2024-11-27 13:52:16 +0000

    bump version for plugin release 0.5.13

--- tools/gradletooling/gradle-plugin/build.gradle.kts
+++ tools/gradletooling/gradle-plugin/build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
 }
 
 group = "net.ivoa.vo-dml"
-version = "0.5.12"
+version = "0.5.13"
 
 repositories {
     mavenLocal() // FIXME remove this when releasing - just here to pick up local vodsl updates
@@ -32,7 +32,7 @@ dependencies {
     implementation("name.dmaus.schxslt:java:3.1.1") // for modern schematron
     implementation("name.dmaus.schxslt:schxslt:1.10") // force to use more updated schematron than the java wrapper naturally uses -
     implementation("org.xmlresolver:xmlresolver:6.0.9") // for xml catalogues - note that the apache xml-commons resolver is out of date
-    implementation("org.javastro.vodsl:vodslparser:0.4.8") //standalone vodsl parser
+    implementation("org.javastro.vodsl:vodslparser:0.4.9") //standalone vodsl parser
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.14.2")
 
 
