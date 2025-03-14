74aeae7e1e7323a019acae988197c63eb6126776
Paul Harrison <paul.harrison@manchester.ac.uk>
2025-03-14 16:36:00 +0000

    update to version 0.5.17

--- tools/gradletooling/gradle-plugin/build.gradle.kts
+++ tools/gradletooling/gradle-plugin/build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
 }
 
 group = "net.ivoa.vo-dml"
-version = "0.5.16"
+version = "0.5.17"
 
 repositories {
     mavenLocal() // FIXME remove this when releasing - just here to pick up local vodsl updates
