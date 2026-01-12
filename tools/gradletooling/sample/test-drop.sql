a5bcd7cc3ee2bc6a545ac290592269bbb8acd530
Paul Harrison <paul.harrison@manchester.ac.uk>
2026-01-12 10:58:26 +0000

    delete files added in error

--- tools/gradletooling/sample/test-drop.sql
+++
@@ -1,8 +0,0 @@
-drop table if exists MyModel.BaseC cascade ;
-drop table if exists MyModel.Refa cascade ;
-drop table if exists MyModel.Refb cascade ;
-drop table if exists MyModel.SomeContent cascade ;
-drop sequence if exists BaseC_SEQ;
-drop sequence if exists Refa_SEQ;
-drop sequence if exists SomeContent_SEQ;
-drop schema MyModel;
